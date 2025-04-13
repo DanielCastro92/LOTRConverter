1ccdd1999b10f3bda595f78fccc77cc049768941
Daniel Castro <daniel.castro92@outlook.com>
2025-04-12 21:04:50 -0300

    Commit Inicial

---
+++ LOTRConverter/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  LOTRConverter
+//
+//  Created by Daniel Castro on 12/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
